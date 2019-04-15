37001b049ea69020e55db0ade577e459c37eb3de
aveal466 <aveal466@student.otago.ac.nz>
2019-04-15 22:03:43 +1200

    Working towards working room api skeletons

--- backend/routes/room.js
+++ backend/routes/room.js
@@ -2,12 +2,11 @@ const express = require('express');
 const server = express();
 const router = require('express-promise-router')();
 
-const { validateBody, schemas } = require('../helpers/routeHelpers'); // calling both things present in the helper file
 const RoomController = require('../controllers/roomController');
 
 //Display room information
 
-router.route('/rooms')
+router.route('/room')
 .get(RoomController.roominfo)
 
 module.exports = router;
