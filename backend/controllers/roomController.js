a97e2d13b81878c74baf0b6aca3361a77f815f94
aveal466 <aveal466@student.otago.ac.nz>
2019-04-18 23:22:47 +1200

    Working on changing SQL to Mongo. References #21

--- backend/controllers/roomController.js
+++ backend/controllers/roomController.js
@@ -1,25 +1,20 @@
 const db = require('../database');
+const Room = require('../models/roomModel')
 
 module.exports = {
 
     //Find list of all rooms from database and display it in JSON form
     roomlist: async (req, res) => {
-        const query = "SELECT * FROM `room`";
-        db.query(query, (error, results, fields) => {
-        if (error) throw error;
-        res.json(results);
+        Room.find({}).then(function(rooms){
+            res.send(rooms);
         });
+        
     },
 
     //Find room details using the room name and display details from database in JSON
     byname: async (req, res) => {
-        const roomname = req.params.name
-        const query = "SELECT * FROM `room` where room_name = ?"
-        console.log("Fetching user with id: " + req.params.name)
-        db.query(query, [roomname], (error, results, fields) => {
-        if (error) throw error;
-        res.json(results);
-    });
+        Room.findOne({req.params.name}).then(function(room){
+            res.send(room);
+        });
     }
-    
- }
+};
