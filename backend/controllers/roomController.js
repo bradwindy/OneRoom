6bfec951e8549163d7fd3700322e04319127f6c7
aveal466 <aveal466@student.otago.ac.nz>
2019-04-19 09:44:01 +1200

    Worked on find room by name mongoose code

--- backend/controllers/roomController.js
+++ backend/controllers/roomController.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
-const db = require('../database');
+//const db = require('../database');
 const Room = require('../models/roomModel')
+const express = require('express');
+const server = express();
 
-=======
->>>>>>> e76ee2419139be9c1efdcae104fe57b5a5f36d2b
 module.exports = {
   //Find list of all rooms from database and display it in JSON form
   roomlist: async (req, res) => {
@@ -14,7 +13,6 @@ module.exports = {
     // });
   },
 
-<<<<<<< HEAD
     //Find list of all rooms from database and display it in JSON form
     roomlist: async (req, res) => {
         Room.find({}).then(function(rooms){
@@ -25,20 +23,8 @@ module.exports = {
 
     //Find room details using the room name and display details from database in JSON
     byname: async (req, res) => {
-        Room.findOne({req.params.name}).then(function(room){
-            res.send(room);
-        });
+      Room.findOne({name:req.params.name}, function(err, room){
+        res.send(room)
+      })   
     }
-};
-=======
-  //Find room details using the room name and display details from database in JSON
-  byname: async (req, res) => {
-    // const roomname = req.params.name
-    // const query = "SELECT * FROM `room` where room_name = ?"
-    // console.log("Fetching user with id: " + req.params.name)
-    // db.query(query, [roomname], (error, results, fields) => {
-    // if (error) throw error;
-    // res.json(results);
-  }
-};
->>>>>>> e76ee2419139be9c1efdcae104fe57b5a5f36d2b
+  };
