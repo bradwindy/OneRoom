b7720f7f1d9741677f8583800c92947e2d8717f0
aveal466 <aveal466@student.otago.ac.nz>
2019-04-30 17:14:12 +1200

    Added get room availability route

--- backend/routes/room.js
+++ backend/routes/room.js
@@ -3,10 +3,15 @@ const server = express();
 const router = require('express-promise-router')();
 const { authJWT } = require('../passport');
 const RoomController = require('../controllers/roomController.js');
+const BookingController = require('../controllers/bookingController.js');
 
 //Get list of rooms from the database
 router.route('/all')
     .get(authJWT, RoomController.allRooms);
+
+//Get list of available rooms from the database
+router.route('/available')
+    .get(authJWT, RoomController.available);
 
 //Get room details based on room names
 router.route('/:name')
