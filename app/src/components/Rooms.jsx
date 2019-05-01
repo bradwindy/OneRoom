3f4510863c59f50d97051be5d67cf765fdbe1f99
Elora Chang <elorachang@outlook.co.nz>
2019-05-02 10:42:24 +1200

    Workign on display rooms function WIP

--- app/src/components/Rooms.jsx
+++ app/src/components/Rooms.jsx
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+//import Room from '/Room';
 
 class Rooms extends Component {
-    state = {
-        rooms: {}
+    constructor(props){
+        super(props);
+
+        //Have just hardcoded data to test
+        this.state = {
+            rooms: [
+                {name: 'GO1', capacity: 5, facilities: 'tv', id: 1234},
+                {name: 'GO2', capacity: 6, facilities: 'tv', id: 1235},
+                {name: 'GO3', capacity: 7, facilities: 'tv', id: 1236}
+
+        ]
     };
+}
 
     componentDidMount() {
         axios.get(`/room/all`)
@@ -15,29 +26,31 @@ class Rooms extends Component {
             })
     };
 
-    
+    //Function to loop through the array of rooms and display them as individual rooms
+    getRoomCards(){
+        if (this.state.rooms.length === 0) return <p>There are no rooms available for this date.</p>;
+
+        return this.state.rooms.map((room, key) => 
+
+            <div className="card m-2" style="width: 18rem;" key={room.id}>
+
+                <div className="card-body">
+                    <h5 className="card-title">`Room: ${this.room.name}`</h5>
+                    <p className="card-text">`Room Capacity: ${this.room.capacity}`</p>
+                    <a href="#" className="btn btn-primary">Learn more</a>
+                </div>
+            </div>
+        )};
+
 
     render() {
-        return (
-            <React.Fragment>
-            <ul>
+    
+        return{}
+            
+             //https://medium.com/javascript-in-plain-english/how-to-loop-through-arrays-in-react-3eaa8a14445
+               //We can implement a new componenet.. {this.state.rooms.map((item, key) => <Room item={item} key={item.id} />
 
-                //https://medium.com/javascript-in-plain-english/how-to-loop-through-arrays-in-react-3eaa8a14445
-            
-            
-);
-                {this.state.rooms.map(rooms => <li key={Rooms.id}>{rooms.name}</li>)}
-            </ul>
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="..."/>
-                 <div class="card-body">
-                    <h5 class="card-title">Room One</h5>
-                    <p class="card-text">This room includes a spacious layout..</p>
-                    <a href="#" class="btn btn-primary">Learn more</a>
-                </div>
-             </div>
-             </React.Fragment>
-        );
+   
         
     }
 }
