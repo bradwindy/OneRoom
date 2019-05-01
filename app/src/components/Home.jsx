f64aca6a12d2bada6e9e6084b02bebe8fa2b040f
Bradley Windybank <trykryda@gmail.com>
2019-05-02 11:00:32 +1200

    Testing displaying JSON in React and using google maps links.

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,58 +1,64 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 
 class Home extends Component {
+    state = {
+        users: []
+    };
+
+    componentDidMount() {
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+            .then(res => {
+                const users = res.data;
+                this.setState({ users });
+            })
+    };
+
     render() {
         return (
             // "Home" component, a scrollable list of cards with booking info and buttons. Just example info for now
             <div className="container pt-4 p-2">
                 <h2 className="pl-3 pb-3 pt-2"><b>My Bookings:</b></h2>
-                <div className="card-columns m-2">
-                    <div className="card">
-                        <div className="card-block m-4">
-                            <h4 className="card-title"><b>Study Group</b></h4>
-                            <ul className="card-text list-unstyled">
-                                <li>
-                                    <b>Date: </b>Sun 10th September
-                                </li>
-                                <li>
-                                    <b>Time: </b>8:00am - 10:00am
-                                </li>
-                                <li>
-                                    <b>Location: </b>Room 12
-                                </li>
-                            </ul>
 
+                { this.state.users.map(
+                    user =>
+                        <div className="card-columns m-2" key={user.id}>
+                            <div className="card">
+                                <div className="card-block m-4">
+                                    <h4 className="card-title"><b>{user.name}</b></h4>
+                                    <ul className="card-text list-unstyled">
+                                        <li>
+                                            <b>Email: </b>{user.email}
+                                        </li>
+                                        <li>
+                                            <b>Website: </b>{user.website}
+                                        </li>
+                                        <li>
+                                            <b>Phone: </b>{user.phone}
+                                        </li>
+                                    </ul>
 
-                            <a href="/" className="btn btn-primary mr-2">Directions</a>
-                            <a href="/" className="btn btn-secondary">More</a>
-                            <a href="/" className="btn btn-danger float-right">Cancel</a>
+                                    <div className="row m-0">
+                                        <button className="btn btn-primary mr-2">Information</button>
+                                        <div className="dropdown">
+                                            <button className="btn btn-secondary dropdown-toggle mr-2" type="button"
+                                                    id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                                    aria-expanded="false">
+                                                More
+                                            </button>
+                                            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                <a href={"http://www.google.com/maps/place/"+ user.address.geo.lat + "," + user.address.geo.lng} className="dropdown-item">Directions</a>
+                                                <a className="dropdown-item" href="">Another action</a>
+                                                <a className="dropdown-item" href="">Something else here</a>
+                                            </div>
+                                        </div>
+                                        <a href="/" className="btn btn-danger float-right">Cancel</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-
-                <div className="card-columns m-2">
-                    <div className="card">
-                        <div className="card-block m-4">
-                            <h4 className="card-title"><b>310 Meeting</b></h4>
-                            <ul className="card-text list-unstyled">
-                                <li>
-                                    <b>Date: </b>Tue 12th September
-                                </li>
-                                <li>
-                                    <b>Time: </b>8:00pm - 9:00pm
-                                </li>
-                                <li>
-                                    <b>Location: </b>Room 2
-                                </li>
-                            </ul>
-
-
-                            <a href="/" className="btn btn-primary mr-2">Directions</a>
-                            <a href="/" className="btn btn-secondary">More</a>
-                            <a href="/" className="btn btn-danger float-right">Cancel</a>
-                        </div>
-                    </div>
-                </div>
+                )}
             </div>
         );
     }
