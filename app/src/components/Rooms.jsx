f397f91868bf75c011ab81c075e68da891bd9ac6
Elora Chang <elorachang@outlook.co.nz>
2019-05-08 12:03:48 +1200

    Room cards completed.

--- app/src/components/Rooms.jsx
+++ app/src/components/Rooms.jsx
@@ -9,48 +9,70 @@ class Rooms extends Component {
         //Have just hardcoded data to test
         this.state = {
             rooms: [
-                {name: 'GO1', capacity: 5, facilities: 'tv', id: 1234},
-                {name: 'GO2', capacity: 6, facilities: 'tv', id: 1235},
-                {name: 'GO3', capacity: 7, facilities: 'tv', id: 1236}
-
-        ]
+                //{id: 1234, name: 'g01', capacity: 4}
+            ]
     };
+
+    this.componentDidMount = this.componentDidMount.bind(this);
 }
 
+
+//decorate the function with async as we are using the await method
+
+async componentDidMount() {
+    const {data: rooms} = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    //pending > resolved (success) or rejected(failure)
+    this.setState({ rooms });
+    
+}
+
+/*
     componentDidMount() {
-        axios.get(`/room/all`)
-            .then(res => {
+        axios.get('https://jsonplaceholder.typicode.com/posts');
+            then(res => {
                 const rooms = res.data;
                 console.log(res);
                 this.setState({ rooms });
             })
-    };
+    };*/
 
     //Function to loop through the array of rooms and display them as individual rooms
     getRoomCards(){
+        
+        
         if (this.state.rooms.length === 0) return <p>There are no rooms available for this date.</p>;
 
+      
         return this.state.rooms.map((room, key) => 
 
-            <div className="card m-2" style="width: 18rem;" key={room.id}>
-
+            <div className="card m-2" key={room.userId}>
                 <div className="card-body">
-                    <h5 className="card-title">`Room: ${this.room.name}`</h5>
-                    <p className="card-text">`Room Capacity: ${this.room.capacity}`</p>
-                    <a href="#" className="btn btn-primary">Learn more</a>
+                    <h5 className="card-title">Room: {room.title}</h5>
+                    <p className="card-text"><b>Room Details:</b> {room.body}</p>
+                    <p className="card-text"><b>Room Capacity</b> {room.id}</p>
+                    <button onClick={this.handleBook} className="btn btn-primary mt-2">Book</button>
                 </div>
             </div>
         )};
 
 
+        handleBook = event => {
+            console.log("Room is booked");
+            
+
+            //handle the booking button to enure that the room is booked. 
+        }
+
+
     render() {
     
-        return{}
+        return(
+            <div>
+                <h3>All rooms available on this date to book:</h3>
+            {this.getRoomCards()}
+                </div>
             
-             //https://medium.com/javascript-in-plain-english/how-to-loop-through-arrays-in-react-3eaa8a14445
-               //We can implement a new componenet.. {this.state.rooms.map((item, key) => <Room item={item} key={item.id} />
-
-   
+        )
         
     }
 }
