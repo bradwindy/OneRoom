174d975b2a8afcf611b0518eab75a80c97b91f88
Elora Chang <elorachang@outlook.co.nz>
2019-05-10 19:16:25 +1200

    git

--- app/src/components/Room.jsx
+++ app/src/components/Room.jsx
@@ -4,14 +4,17 @@ class Room extends Component {
 
     render() { 
         return ( 
-             <div className="card m-2" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="..."/>
-                 <div className="card-body">
-                    <h5 className="card-title">`Room: ${this.props.room.name}`</h5>
-                    <p className="card-text">`Room Capacity: ${this.props.room.capacity}`</p>
-                    <a href="#" className="btn btn-primary">Learn more</a>
+
+
+             <div className="card m-2" key={this.props.id}>
+                <div className="card-body">
+                    <h5 className="card-title">Room: {this.props.name}</h5>
+                    <p className="card-text"><b>Room Capacity:</b> {this.props.capacity}</p>          
+                    <p className="card-text"><b>Room Facilities</b> {this.props.facilities.tv}</p>
+                    <p className="card-text"><b>Available</b> {this.props.available}</p>
+                    <button onClick={() => this.props.onBook(this.props.id)} className="btn btn-primary mt-2">Book</button>
                 </div>
-             </div>
+            </div>
        
                 
          );
--- app/src/components/Rooms.jsx
+++ app/src/components/Rooms.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 
-//import Room from '/Room';
+import Room from './Room';
 
 class Rooms extends Component {
     constructor(props){
@@ -13,51 +13,69 @@ class Rooms extends Component {
     };
 
     this.componentDidMount = this.componentDidMount.bind(this);
+    
 }
 
 
 //decorate the function with async as we are using the await method
-
 async componentDidMount() {
     const {data: rooms} = await axios.get('/room/all');
     //pending > resolved (success) or rejected(failure)
     this.setState({ rooms });  
 }
 
+//Handle booking - recieves the room id of the room to be booked. Passes this through. 
+handleBook = (roomId) => {
+    console.log("Room is booked");
+    axios.patch('/booking', {roomiD: roomId});
+    console.log("Room Id: ", roomId);
+
+    //Update the rooms list to not show the room that has been booked. 
+    //This could be a backend thing. 
+    //const rooms = this.state.rooms.filter(r => r.id !== room.id);
+    //this.setState({rooms});
+    
+};
+
     //Function to loop through the array of rooms and display them as individual rooms
     getRoomCards(){
         
         if (this.state.rooms.length === 0) return <p>There are no rooms available for this date.</p>;
 
-      
+
+        //This is if we want to keep it all within the same class. 
         return this.state.rooms.map((room, key) => 
 
-            <div className="card m-2" key={room.name}>
+            <div className="card m-2" key={room.id}>
                 <div className="card-body">
                     <h5 className="card-title">Room: {room.name}</h5>
                     <p className="card-text"><b>Room Capacity:</b> {room.capacity}</p>          
                     <p className="card-text"><b>Room Facilities</b> {room.facilities.tv}</p>
-                    <button onClick={this.handleBook} className="btn btn-primary mt-2">Book</button>
+                    <p className="card-text"><b>Available</b> {room.available}</p>
+                    <button onClick={ () => this.handleBook(room.id)} className="btn btn-primary mt-2">Book</button>
                 </div>
+                
             </div>
-        )};
 
-        handleBook = event => {
-            console.log("Room is booked");
             /*
-            //To send the room booked to the backend. 
-            axios.patch('/booking', {roomId: room.id});
-            //handle the booking button to enure that the room is booked. 
-            console.log("Room Booked");*/
-        }
-
+              {this.state.rooms.map(room => (
+                    <Room 
+                    key={Room.id} 
+                    onBook={this.handleBook} 
+                    name={Room.name} 
+                    capacity={Room.capacity}  
+                    />
+                ))};*/
+        )
+        };
 
     render() {
     
         return(
             <div>
                 <h3>All rooms available on this date to book:</h3>
-            {this.getRoomCards()}
+                {this.getRoomCards()}
+
                 </div>
             
         )
