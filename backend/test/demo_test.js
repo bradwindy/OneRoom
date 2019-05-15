26c5769c5d17a345dbde64f99ee7844b12fcd082
Akash Mokashi <Akash@Akashs-MBP.student.uod.otago.ac.nz>
2019-05-15 22:19:23 +1200

    Setup of tests

---
+++ backend/test/demo_test.js
@@ -0,0 +1,12 @@
+const mocha = require('mocha');
+const assert = require('assert');
+
+// Describe tests
+describe('some demo tests', function() {
+
+    // Create tests
+    it('adds two numbers together', function() {
+        assert(2 + 4 === 5);
+    });
+
+});
