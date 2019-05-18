01e969e64847b59074bf381fc096f389a398442a
Elora Chang <elorachang@outlook.co.nz>
2019-05-19 04:26:31 +1200

    Nav Bar updated.

--- app/src/components/Nav.jsx
+++ app/src/components/Nav.jsx
@@ -23,7 +23,7 @@ class Nav extends Component {
                     RoomEase
                   </a>
                   <button
-                    class="navbar-toggler"
+                    className="navbar-toggler"
                     type="button"
                     data-toggle="collapse"
                     data-target="#navbarNavAltMarkup"
@@ -38,55 +38,81 @@ class Nav extends Component {
                     id="navbarNavAltMarkup"
                   >
                     <div class="navbar-nav">
-                      <a class="nav-item nav-link active" href="/login">
+                      <a class="nav-item nav-link" href="/login">
                         Login <span class="sr-only">(current)</span>
                       </a>
                       <a class="nav-item nav-link" href="/">
-                        Features
+                        My Bookings
                       </a>
-                      <a class="nav-item nav-link" href="/">
-                        Pricing
+                      <a class="nav-item nav-link" href="/book/date">
+                        Book <span className="sr-only">(current)</span>
                       </a>
-                      <a
-                        class="nav-item nav-link disabled"
-                        href="/"
-                      >
-                        Disabled
+                      <a class="nav-item nav-link" href="/login" onClick={Nav.logout}>
+                        Logout <span className="sr-only">(current)</span>
                       </a>
+                      
+                      
                     </div>
                   </div>
                 </nav>
               </header>
 
-              /*<ul className="navbar-nav bd-navbar-nav">
-                            <li className="nav-item">
-                                
-                            </li>
-                            <li className="nav-item nav-link active">
-                                <a className="btn btn-outline-secondary mr-2" href="/login" role="button">Log In</a>
-                            </li>
-                            <li className="nav-item">
-                                <button className="btn btn-outline-danger mr-2" onClick={Nav.logout}>Log Out</button>
-                            </li>
-                            <li className="nav-item">
-                                <a className="btn btn-primary" href="/" role="button">Home</a>
-                            </li>
-                        </ul>
-                    </div>
-                    </nav>
-                </header>*/
+        
             );
         } else {
             // noinspection HtmlUnknownTarget
             return (
+
+              <header>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                  <a className="navbar-brand" href="/">
+                    RoomEase
+                  </a>
+                  <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                  >
+                    <span class="navbar-toggler-icon" />
+                  </button>
+                  <div
+                    class="collapse navbar-collapse"
+                    id="navbarNavAltMarkup"
+                  >
+                    <div class="navbar-nav">
+                      <a class="nav-item nav-link" href="/login">
+                        Login <span className="sr-only">(current)</span>
+                      </a>
+                      <a class="nav-item nav-link" href="/">
+                        My Bookings
+                      </a>
+                      <a class="nav-item nav-link " href="/book/date">
+                        Book <span className="sr-only">(current)</span>
+                      </a>
+                      <a class="nav-item nav-link" href="/login" onClick={Nav.logout}>
+                        Logout <span className="sr-only">(current)</span>
+                      </a>
+                      
+                      
+                    </div>
+                  </div>
+                </nav>
+              </header>
+
+
                 // nav bar component, will update later on to dynamically render its elements depending on if the user is
                 // logged in or not.
+                /*
                 <header
                     className="navbar navbar-expand navbar-light bg-light ">
                     <div className="navbar-nav-scroll">
                         <ul className="navbar-nav bd-navbar-nav">
                             <li className="nav-item">
-                                <a className="navbar-brand" href="/"><b>RoomEaseImg</b></a>
+                                <a className="navbar-brand" href="/"><b>RoomEase</b></a>
                             </li>
                             <li className="nav-item nav-link active">
                                 <a className="btn btn-outline-secondary mr-2" href="/login" role="button">Log In</a>
@@ -99,7 +125,7 @@ class Nav extends Component {
                             </li>
                         </ul>
                     </div>
-                </header>
+                </header>*/
             );
         }
     }
