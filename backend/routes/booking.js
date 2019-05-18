0c3e0f01490ad79f6a82714f06055e61208d6b21
VG <vg@VGs-MacBook-Pro.student.uod.otago.ac.nz>
2019-05-18 17:58:53 +1200

    Delete booking feature implemented

--- backend/routes/booking.js
+++ backend/routes/booking.js
@@ -32,7 +32,7 @@ router.route('/editBooking')
     .put(BookingController.edit);
 
 // DELETE a booking
-server.route("/deleteBooking")
+router.route('/deleteBooking/:roomId/:bookingId')
     .delete(BookingController.delete);
     /** Delete Logic Flow
      *  1. Look for the item, if not existing, return 404 and exit route.
