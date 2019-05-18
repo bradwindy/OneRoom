19e16e9592aad3cc3d64131346541111b9de8e12
Akash Mokashi <Akash@Akashs-MBP.student.uod.otago.ac.nz>
2019-05-19 02:45:00 +1200

    Added styling and images to front end.

--- app/src/components/Register.jsx
+++ app/src/components/Register.jsx
@@ -7,6 +7,7 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import FormValidator from "./FormValidator";
+import Background from "../images/background_profile.png";
 
 class Register extends Component {
     state = {
