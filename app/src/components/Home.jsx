f28e201e2cff06435a5bc47b44d4cfec08f24a15
Elora Chang <elorachang@outlook.co.nz>
2019-05-20 09:15:41 +1200

    Updated the room bookings page alert and redirection.

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,106 +1,129 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import Background from "../images/background_two.png";
 import BackgroundTwo from "../images/background_three.png";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            bookings: [],
-            interpretedAlert: null,
-        };
-
-        // This binding is necessary to make `this` work in the callback
-        this.cancelBooking = this.cancelBooking.bind(this);
-        this.cancelConfirm = this.cancelConfirm.bind(this);
-        this.componentDidMount = this.componentDidMount.bind(this);
-    }
-
-    componentDidMount = () => {
-        let bookingArr = JSON.parse(localStorage.getItem('bookingArray'));
-
-        if (bookingArr == null) {
-            bookingArr = [];
-        }
-
-        this.setState({bookings: bookingArr})
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookings: [],
+      interpretedAlert: null
     };
 
-    cancelBooking = async (bookingId) => {
-        this.props.removeBookingFunc(bookingId);
+    // This binding is necessary to make `this` work in the callback
+    this.cancelBooking = this.cancelBooking.bind(this);
+    this.cancelConfirm = this.cancelConfirm.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
 
-        /*await axios.delete(`https://jsonplaceholder.typicode.com/users/${bookingId}`)
+  componentDidMount = () => {
+    let bookingArr = JSON.parse(localStorage.getItem("bookingArray"));
+
+    if (bookingArr == null) {
+      bookingArr = [];
+    }
+
+    this.setState({ bookings: bookingArr });
+  };
+
+  cancelBooking = async bookingId => {
+    this.props.removeBookingFunc(bookingId);
+
+    /*await axios.delete(`https://jsonplaceholder.typicode.com/users/${bookingId}`)
             .then(res => {
                 console.log(res);
             });*/
 
-        window.location.reload();
-    };
+    window.location.reload();
+  };
 
-    cancelConfirm = () => {
+  cancelConfirm = () => {};
 
-    };
+  render() {
+    if (this.state.bookings === undefined || this.state.bookings.length === 0) {
+      return (
+        <div
+          className="container pt-4 p-2"
+          style={{
+            backgroundImage: "url(" + Background + ")",
+            backgroundPosition: "bottom center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "60%",
+            height: "700px"
+          }}
+        >
+        
 
-    render() {
-        if (this.state.bookings === undefined || this.state.bookings.length === 0) {
-            return (
-                <div className="container pt-4 p-2" style={{
-                    backgroundImage: "url(" + Background + ")",
-                    backgroundPosition: "bottom center",
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "60%",
-                    height: "700px"
-                  }}>
-                    <h2 className="pl-3 pb-1 pt-2 font-weight-bold">My Bookings:</h2>
-                    <h4 className="pl-3 pb-3 pt-2">You currently have no bookings.</h4>
+          <div class="card text-center">
+           
+            <div class="card-body">
+              <h5 class="card-title">You currently have no bookings.</h5>
+              <p class="card-text">
+                Don't miss out! Book your room now. 
+              </p>
+              <a href="/book/date" class="btn btn-primary">
+                Book Room
+              </a>
+            </div>
+            
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        // "Home" component, a scrollable list of cards with booking info and buttons. Just example info for now
+        <div
+          className="container pt-4 p-2"
+          style={{
+            backgroundImage: "url(" + BackgroundTwo + ")",
+            backgroundPosition: "bottom center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "90%",
+            height: "700px"
+          }}
+        >
+          <h2 className="pl-3 pb-3 pt-2 font-weight-bold">My Bookings:</h2>
+
+          {this.state.bookings.map(booking => (
+            <div className="card m-2" key={booking.bookingId}>
+              <div className="card-block m-4">
+                <h4 className="card-title">
+                  <b>{booking.bookingName}</b>
+                </h4>
+                <ul className="card-text list-unstyled">
+                  <li>
+                    <b>Date: </b>
+                    {booking.bookingDate}
+                  </li>
+                  <li>
+                    <b>Time: </b>
+                    {booking.time}
+                  </li>
+                  <li>
+                    <b>Room: </b>
+                    {booking.bookingRoomName}
+                  </li>
+                </ul>
+
+                <div className="row m-0">
+                  <button
+                    className="btn btn-sm btn-outline-danger float-right"
+                    onClick={() => {
+                      // noinspection JSIgnoredPromiseFromCall
+                      this.cancelBooking(booking.bookingId);
+                    }}
+                  >
+                    <FontAwesomeIcon icon="times" /> Cancel
+                  </button>
                 </div>
-            );
-
-        } else {
-            return (
-                // "Home" component, a scrollable list of cards with booking info and buttons. Just example info for now
-                <div className="container pt-4 p-2" style={{
-                    backgroundImage: "url(" + BackgroundTwo + ")",
-                    backgroundPosition: "bottom center",
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "90%",
-                    height: "700px"
-                  }}>
-                    <h2 className="pl-3 pb-3 pt-2 font-weight-bold">My Bookings:</h2>
-
-                    {this.state.bookings.map(booking =>
-
-                        <div className="card m-2" key={booking.bookingId}>
-                            <div className="card-block m-4">
-                                <h4 className="card-title"><b>{booking.bookingName}</b></h4>
-                                <ul className="card-text list-unstyled">
-                                    <li>
-                                        <b>Date: </b>{booking.bookingDate}
-                                    </li>
-                                    <li>
-                                        <b>Time: </b>{booking.time}
-                                    </li>
-                                    <li>
-                                        <b>Room: </b>{booking.bookingRoomName}
-                                    </li>
-                                </ul>
-
-                                <div className="row m-0">
-                                    <button className="btn btn-sm btn-outline-danger float-right" onClick={() => {
-                                        // noinspection JSIgnoredPromiseFromCall
-                                        this.cancelBooking(booking.bookingId)
-                                    }}><FontAwesomeIcon icon="times"/> Cancel
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    )}
-                </div>
-            );
-        }
+              </div>
+            </div>
+          ))}
+        </div>
+      );
     }
+  }
 }
 
 export default Home;
