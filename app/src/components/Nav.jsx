220fbfd0c15086592388b3bba3ba34278295de31
Bradley Windybank <trykryda@gmail.com>
2019-05-20 22:31:04 +1200

    Minor changes.

--- app/src/components/Nav.jsx
+++ app/src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {withRouter} from 'react-router-dom';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
+//import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 
 class Nav extends Component {
